e747caf0895017133abaf9ea347f8f4bf32dece9
yogesh-1303 <77864026+yogesh-1303@users.noreply.github.com>
2021-02-22 10:08:31 +0530

    Add files via upload

---
+++ RUN/source/Blinking_LED.d
@@ -0,0 +1,24 @@
+source/Blinking_LED.o source/Blinking_LED.d: ../source/Blinking_LED.c \
+ C:\CU_Files\MCUXpressoIDE_11.2.1_4149\workspace\Blinkenlights\CMSIS/MKL25Z4.h \
+ C:\CU_Files\MCUXpressoIDE_11.2.1_4149\workspace\Blinkenlights\CMSIS/core_cm0plus.h \
+ C:\CU_Files\MCUXpressoIDE_11.2.1_4149\workspace\Blinkenlights\CMSIS/core_cmInstr.h \
+ C:\CU_Files\MCUXpressoIDE_11.2.1_4149\workspace\Blinkenlights\CMSIS/cmsis_gcc.h \
+ C:\CU_Files\MCUXpressoIDE_11.2.1_4149\workspace\Blinkenlights\CMSIS/core_cmFunc.h \
+ C:\CU_Files\MCUXpressoIDE_11.2.1_4149\workspace\Blinkenlights\CMSIS/system_MKL25Z4.h \
+ ../source/tsi.h ../source/led.h
+
+C:\CU_Files\MCUXpressoIDE_11.2.1_4149\workspace\Blinkenlights\CMSIS/MKL25Z4.h:
+
+C:\CU_Files\MCUXpressoIDE_11.2.1_4149\workspace\Blinkenlights\CMSIS/core_cm0plus.h:
+
+C:\CU_Files\MCUXpressoIDE_11.2.1_4149\workspace\Blinkenlights\CMSIS/core_cmInstr.h:
+
+C:\CU_Files\MCUXpressoIDE_11.2.1_4149\workspace\Blinkenlights\CMSIS/cmsis_gcc.h:
+
+C:\CU_Files\MCUXpressoIDE_11.2.1_4149\workspace\Blinkenlights\CMSIS/core_cmFunc.h:
+
+C:\CU_Files\MCUXpressoIDE_11.2.1_4149\workspace\Blinkenlights\CMSIS/system_MKL25Z4.h:
+
+../source/tsi.h:
+
+../source/led.h:
---
+++ source/led.h
@@ -0,0 +1,4 @@
+void def();
+void led_init();
+void led_on(int num);
+void led_off();
